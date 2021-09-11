6be0193a7e2ae3c5ea9934f07e19974b3c0767c3
Ivan da Silva Rosa <iivan.si@msn.com>
2021-09-11 10:51:13 -0300

    Add Material Icons cdn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
+    <!-- Material Design Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
     <title>E-Diaristas</title>
   </head>
   <body>
